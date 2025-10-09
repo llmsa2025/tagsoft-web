cc7a60b7b40d2ef82e5894d26c8c29932d8b340d
llmsa2025 <luan.limanm@gmail.com>
2025-10-09 09:33:01 -0400

    Create auth.js

---
+++ scr/lib/auth.js
@@ -0,0 +1,5 @@
+// Stub de autenticação/RBAC para o futuro
+export function useSession() {
+  // No futuro, retornar usuário/sessão/roles
+  return { user: { name: 'Usuário' }, roles: ['owner'] };
+}
