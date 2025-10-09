5ead3db4a569e679564a13a690e8f9fe94eb2740
llmsa2025 <luan.limanm@gmail.com>
2025-10-09 14:03:11 -0400

    Create index.ts

---
+++ scr/capture/schemas/index.ts
@@ -0,0 +1,7 @@
+export * from './eventBase';
+export * from './session';
+export * from './screen';
+export * from './action';
+export * from './error';
+export * from './performance';
+export * from './milestone';
